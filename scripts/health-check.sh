c08139b6ee4d01b18b022dadf4ff92e18d2d3066
hozlucas28 <hozlucas28@gmail.com>
2026-02-03 19:15:03 +0000

    refactor: health check

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -66,113 +66,105 @@ fi
 exit_code=0
 
 
-# ------------------------------- Common Tools ------------------------------- #
-
-echo -e "\e[90mChecking common tools...\n\e[0m"
+if command -v go >/dev/null 2>&1; then
+	echo -e "\e[32m- Go $(command_version go version) installed.\e[0m"
+else
+	echo -e "\e[31m- Go is not installed or not found in PATH.\e[0m" >&2
+	exit_code=1
+fi
 
 if command -v node >/dev/null 2>&1; then
-	echo -e "\e[32m  - Node.js $(command_version node --version) installed.\e[0m"
+	echo -e "\e[32m- Node.js $(command_version node --version) installed.\e[0m"
 else
-	echo -e "\e[31m  - Node.js is not installed or not found in PATH.\e[0m" >&2
+	echo -e "\e[31m- Node.js is not installed or not found in PATH.\e[0m" >&2
 	exit_code=1
 fi
 
 if command -v bun >/dev/null 2>&1; then
-	echo -e "\e[32m  - Bun $(command_version bun --version) installed.\e[0m"
+	echo -e "\e[32m- Bun $(command_version bun --version) installed.\e[0m"
 else
-	echo -e "\e[31m  - Bun is not installed or not found in PATH.\e[0m" >&2
+	echo -e "\e[31m- Bun is not installed or not found in PATH.\e[0m" >&2
 	exit_code=1
 fi
 
-if command -v renovate >/dev/null 2>&1; then
-	echo -e "\e[32m  - Renovate $(command_version act --version) installed.\e[0m"
+if command -v golangci-lint >/dev/null 2>&1; then
+	echo -e "\e[32m- Golangci-lint $(command_version golangci-lint version) installed.\e[0m"
 else
-	echo -e "\e[31m  - Renovate is not installed or not found in PATH.\e[0m" >&2
-	exit_code=1
-fi
-
-if command -v act >/dev/null 2>&1; then
-	echo -e "\e[32m  - act $(command_version act --version) installed.\e[0m"
-else
-	echo -e "\e[31m  - act is not installed or not found in PATH.\e[0m" >&2
+	echo -e "\e[33m  - Golangci-lint is not installed or not found in PATH.\e[0m" >&2
 	exit_code=1
 fi
 
 if command -v gitleaks >/dev/null 2>&1; then
-	echo -e "\e[32m  - Gitleaks $(command_version gitleaks --version) installed.\e[0m"
+	echo -e "\e[32m- Gitleaks $(command_version gitleaks --version) installed.\e[0m"
 else
-	echo -e "\e[31m  - Gitleaks is not installed or not found in PATH.\e[0m" >&2
+	echo -e "\e[31m- Gitleaks is not installed or not found in PATH.\e[0m" >&2
+	exit_code=1
+fi
+
+if command -v renovate >/dev/null 2>&1; then
+	echo -e "\e[32m- Renovate $(command_version act --version) installed.\e[0m"
+else
+	echo -e "\e[31m- Renovate is not installed or not found in PATH.\e[0m" >&2
+	exit_code=1
+fi
+
+if command -v act >/dev/null 2>&1; then
+	echo -e "\e[32m- act $(command_version act --version) installed.\e[0m"
+else
+	echo -e "\e[31m- act is not installed or not found in PATH.\e[0m" >&2
 	exit_code=1
 fi
 
 if command -v gh >/dev/null 2>&1; then
-	echo -e "\e[32m  - GitHub CLI $(command_version gh --version) installed.\e[0m"
+	echo -e "\e[32m- GitHub CLI $(command_version gh --version) installed.\e[0m"
 else
-	echo -e "\e[31m  - GitHub CLI is not installed or not found in PATH.\e[0m" >&2
+	echo -e "\e[31m- GitHub CLI is not installed or not found in PATH.\e[0m" >&2
 	exit_code=1
 fi
 
 if [[ "$env" == "local" ]]; then
 	if command -v jq >/dev/null 2>&1; then
-		echo -e "\e[32m  - jq $(command_version jq --version) installed.\e[0m"
+		echo -e "\e[32m- jq $(command_version jq --version) installed.\e[0m"
 	else
-		echo -e "\e[31m  - jq is not installed or not found in PATH.\e[0m" >&2
+		echo -e "\e[31m- jq is not installed or not found in PATH.\e[0m" >&2
 		exit_code=1
 	fi
 fi
 
 if command -v docker >/dev/null 2>&1; then
-	echo -e "\e[32m  - Docker $(command_version docker --version) installed.\e[0m"
+	echo -e "\e[32m- Docker $(command_version docker --version) installed.\e[0m"
 else
-	echo -e "\e[31m  - Docker is not installed, not found in PATH, or not running.\e[0m" >&2
+	echo -e "\e[31m- Docker is not installed, not found in PATH, or not running.\e[0m" >&2
 	exit_code=1
 fi
 
 if version=$(command_version bun run prettier --version); then
-	echo -e "\e[32m  - Prettier $version installed.\e[0m"
+	echo -e "\e[32m- Prettier $version installed.\e[0m"
 else
-	echo -e "\e[31m  - Prettier is not installed.\e[0m" >&2
+	echo -e "\e[31m- Prettier is not installed.\e[0m" >&2
 	exit_code=1
 fi
 
 if version=$(command_version bun run biome --version); then
-	echo -e "\e[32m  - Biome $version installed.\e[0m"
+	echo -e "\e[32m- Biome $version installed.\e[0m"
 else
-	echo -e "\e[31m  - Biome is not installed.\e[0m" >&2
+	echo -e "\e[31m- Biome is not installed.\e[0m" >&2
 	exit_code=1
 fi
 
 if version=$(command_version bun run lefthook --version); then
-	echo -e "\e[32m  - Lefthook $version installed.\e[0m"
+	echo -e "\e[32m- Lefthook $version installed.\e[0m"
 else
-	echo -e "\e[31m  - Lefthook is not installed.\e[0m" >&2
+	echo -e "\e[31m- Lefthook is not installed.\e[0m" >&2
 	exit_code=1
 fi
 
 if command -v zizmor >/dev/null 2>&1; then
-	echo -e "\e[32m  - Zizmor $(command_version zizmor --version) installed.\e[0m"
+	echo -e "\e[32m- Zizmor $(command_version zizmor --version) installed.\e[0m"
 else
-	echo -e "\e[31m  - Zizmor is not installed or not found in PATH.\e[0m" >&2
+	echo -e "\e[31m- Zizmor is not installed or not found in PATH.\e[0m" >&2
 	exit_code=1
 fi
 
-
-# -------------------------- Dedicated Backend Tools ------------------------- #
-
-echo -e "\e[90m\nChecking dedicated Backend tools...\n\e[0m"
-
-if command -v go >/dev/null 2>&1; then
-	echo -e "\e[32m  - Go $(command_version go version) installed.\e[0m"
-else
-	echo -e "\e[31m  - Go is not installed or not found in PATH.\e[0m" >&2
-	exit_code=1
-fi
-
-if command -v golangci-lint >/dev/null 2>&1; then
-	echo -e "\e[32m  - Golangci-lint $(command_version golangci-lint version) installed.\e[0m"
-else
-	echo -e "\e[33m  - Golangci-lint is not installed or not found in PATH.\e[0m" >&2
-	exit_code=1
-fi
 
 exit $exit_code
